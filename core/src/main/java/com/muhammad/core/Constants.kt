c84ffd1b0f9c5e177dd0209ecf3d04cf09bd828a
Muhammad Bilal <160990871+codewithbilalahmad@users.noreply.github.com>
2025-07-04 08:58:15 +0500

    Update Constants.kt

--- core/src/main/java/com/muhammad/core/Constants.kt
+++ core/src/main/java/com/muhammad/core/Constants.kt
@@ -3,6 +3,6 @@ package com.muhammad.core
 object Constants {
     const val TIKTOK_BASE_URL = "https://www.googleapis.com/youtube/v3/"
     const val UNSPLASH_BASE_URL = "https://api.unsplash.com/"
-    const val TIKTOK_API_KEY = "AIzaSyCiGqk3JYBexrE81LlwEj_r2Lp0oiOkJ2U"
-    const val UNSPLASH_API_KEY = "5mEO02Dj45TnLjfnWwJWmbXUu6p-sDMwdyvxoI0q49E"
+    const val TIKTOK_API_KEY = "YOUR YOUTUBE API KEY"
+    const val UNSPLASH_API_KEY = "YOUR UNSPLASH API KEY"
 }
